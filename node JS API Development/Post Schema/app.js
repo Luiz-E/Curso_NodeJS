1ae5f00956d7f82d1281669e1c3ac21c26e50703
Luiz Eduardo <luizedumartins21@gmail.com>
2022-09-06 17:53:01 -0300

    Relembrando banco de dados

---
+++ node JS API Development/Post Schema/app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+const dotenv = require("dotenv")
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => {
+    console.log("DB connected")
+})
+
+mongoose.connection.on("error", (err) => {
+    console.log(err.message)
+})
+
+const postRoutes = require("./routes/post")
+
+app.use(morgan("dev"))
+app.use("/", postRoutes)
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running")
+})
